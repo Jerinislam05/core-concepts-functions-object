3cf79c9a4fb8a3704634a597f270627f2a8cbb85
Jerinislam05 <jerinislam05@gmail.com>
2023-10-18 21:40:18 +0100

    core concepts functions object

---
+++ recap.js
@@ -0,0 +1,6 @@
+/* 
+#js is a scripting language that allows to implement complex features on web pages
+#can also be said as a programming language for the web 
+# variable holo ekti container
+#premitive data types holo number,string,boolean,null,undefined. non-primitive types holo objects, arrays
+*/
